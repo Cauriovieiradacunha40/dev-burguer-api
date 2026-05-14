630be2b2d9dafa0c4685bb2e59fab84e1cc42fe5
cauriovieiradacunha40 <cauriov3@gmail.com>
2026-05-13 21:23:23 -0300

    chore: initial backend setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+import express from 'express';  // Import the Express library
+
+const app = express();          // variavel = express, passa a ser..
+
+export default app;             // exporta a variavel app para ser usada em outros arquivos
+
+
+
+
+
+
---
+++ src/server.js
@@ -0,0 +1,8 @@
+
+import app from './app.js';  // Importa a variável app do arquivo app.js
+
+app.listen(3001, () => console.log("aplication running on port 3001")); // Inicia o servidor na porta 3001 e exibe uma mensagem no console quando estiver rodando
+
+
+
+ 
