28860bef365f0bed8fdd5a9920b0edc3013de0cd
cauriovieiradacunha40 <cauriov3@gmail.com>
2026-06-02 06:16:18 -0300

    feat: add user model, database initialization and mvc structure

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 
-import app from './app.js';  // Importa a variável app do arquivo app.js
+import app from './app.js';            // Importa a variável app do arquivo app.js
+import './database/index.js';          // Importa o arquivo de configuração do banco de dados para estabelecer a conexão
 
-app.listen(3001, () => console.log("aplication running on port 3001")); // Inicia o servidor na porta 3001 e exibe uma mensagem no console quando estiver rodando
+app.listen(3001, () => console.log("aplication running on port 3001"));      // Inicia o servidor na porta 3001 e exibe uma mensagem no console quando estiver rodando
 
 
 
