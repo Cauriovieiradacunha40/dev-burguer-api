b40e44549f38b161157dab7da205ad9037be90c2
cauriovieiradacunha40 <cauriov3@gmail.com>
2026-06-04 00:01:02 -0300

    feat: add routes file and uuid integration

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
+import express from 'express'; // Import the Express library
+import routes from './routes.js'; 
 
-import express from 'express';  // Import the Express library
-
-const app = express();          // variavel = express, passa a ser..
-
-export default app;             // exporta a variavel app para ser usada em outros arquivos
-
-
-
-
-
-
+const app = express(); // variavel = express, passa a ser..
+app.use(routes);    
+app.use(express.json()); // Middleware para interpretar o corpo das requisições como JSON
+export default app; // exporta a variavel app para ser usada em outros arquivos
